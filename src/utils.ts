60ca39403ea230edcdb5fd0d020bdab49255e9a7
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2021-10-13 16:17:31 -0400

    Initial commit

---
+++ src/utils.ts
@@ -0,0 +1,14 @@
+import { connect, ConnectionOptions } from "mongoose"
+const {
+    // Attempts to connect to MongoDB and then tries to connect locally:)
+    MONGO_URI = "mongodb://localhost:27017/next_test"
+} = process.env
+
+console.log(MONGO_URI)
+
+const options: ConnectionOptions = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}
+
+export const connectToDatabase = () => connect(MONGO_URI, options)
